2cb3b98a55761c4a29b600d5b073838c970da1e3
Josep Natanael Pasaribu <joseppasaribu1@gmail.com>
2026-07-16 22:59:12 +0700

    Set up ARUS project shell and dependencies
    Rename package to arus, install leaflet/react-leaflet/@turf/turf,
    dark-theme shell layout, remove unused default template assets.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>ARUS</h1>
+        <span className="tagline">Flood-aware pedestrian routes for Jakarta</span>
+      </header>
+      <div className="map-area" />
+    </div>
+  )
+}
+
+export default App
